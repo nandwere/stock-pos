f93a629ed00b625bea4e130a823d1cadb904cdac
nandwere <mnandwere@gmail.com>
2026-05-24 08:11:17 +0300

    Pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,17 +5,20 @@ module.exports = {
     args: 'start',
     cwd: '/home/app/apps/stock-pos',
     instances: 1,
-    exec_mode: 'cluster',
+    exec_mode: 'fork',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
     },
     max_memory_restart: '500M',
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
+    error_file: '/home/app/apps/logs/err.log', // Use absolute path
+    out_file: '/home/app/apps/logs/out.log',  // Use absolute path
     log_date_format: 'YYYY-MM-DD HH:mm Z',
     merge_logs: true,
     autorestart: true,
-    watch: false
+    watch: false,
+    kill_timeout: 5000,
+    wait_ready: true,
+    listen_timeout: 10000
   }]
 };
